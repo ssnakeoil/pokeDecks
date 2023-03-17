f80a7b74f4638729a33c37112394382780fe55a0
jessikea <jessikea@jtran.git>
2023-03-16 20:51:37 -0400

    god bless Ringo. thank you king

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -12,13 +12,93 @@ const typeDefs = gql`
         token: ID!
         user: User
     }
+    type Query {
+        me: User
+        card(name: String!): [Card]
+    }
 
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(bookData: BookInput!): User
-        removeBook(bookId: ID!): User
       }
+
+      type Holofoil {
+        low: Int
+        mid: Float
+        high: Float
+        market: Float
+        directLow: Float
+      }
+      
+      type Prices {
+        holofoil: Holofoil
+      }
+      
+      type Tcgplayer {
+        url: String
+        updatedAt: String
+        prices: Prices
+      }
+      
+      type Images {
+        small: String
+        large: String
+      }
+      
+      type Legalities {
+        unlimited: String
+        expanded: String
+      }
+      
+      type Set {
+        id: String
+        name: String
+        series: String
+        printedTotal: Int
+        total: Int
+        ptcgoCode: String
+        releaseDate: String
+        updatedAt: String
+        images: Images
+        legalities: Legalities
+      }
+      
+      type Weaknesses {
+        type: String
+        value: String
+      }
+      
+      type Attacks {
+        name: String
+        convertedEnergyCost: Int
+        damage: String
+        text: String
+        cost: [String]
+      }
+      
+      type Card {
+        id: String
+        name: String
+        supertype: String
+        hp: String
+        convertedRetreatCost: Int
+        number: String
+        artist: String
+        rarity: String
+        tcgplayer: Tcgplayer
+        images: Images
+        legalities: Legalities
+        nationalPokedexNumbers: [Int]
+        set: Set
+        retreatCost: [String]
+        weaknesses: [Weaknesses]
+        attacks: [Attacks]
+        rules: [String]
+        evolvesTo: [String]
+        types: [String]
+        subtypes: [String]
+      }
+    
 
 `;
 
---
+++ server/utils/tcgPokemon.js
@@ -0,0 +1,21 @@
+const pokemon =require( 'pokemontcgsdk')
+
+pokemon.configure({ apiKey: process.env.POKECARD_API_KEY })
+
+const findCardbyName = async (name) => {
+  console.log(name)
+    try {
+        const result = await pokemon.card.where({ q: `name:${name}` })
+        // .then(result => {
+        //     console.log(result.data[0].name) // "Blastoise"
+        // })
+        console.log(result.data)
+        return result.data
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+}
+
+module.exports = { findCardbyName }
