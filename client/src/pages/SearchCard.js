3273ef829eed5a74d8a43c9ff34e83b6b7d464cc
FrancisLao9 <lao.francis@gmail.com>
2023-03-20 20:22:30 -0400

    Card Collection and Card File Thingy

---
+++ client/src/pages/SearchCard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Container,
+    Col,
+    Form,
+    Button,
+    Card,
+    Row
+} from 'react-bootstrap';
+
+import Auth from '../utils/auth';
